990fee6dba51246192a099da29abbf3d8f9a26a1
BuildTools <jhaigh1997@gmail.com>
2019-07-24 11:24:51 +1000

    Better comparison for cards in the same strength category

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -51,18 +51,62 @@ public class Card {
 
         //Compare cards based on the hand strength code
         //what is classified as a "weak card"?
-        //it depends on the current suit and situation that the card can be played
 
         //example:
         //what card seems weaker when hearts are trumps?
         // {7 Hearts} or {Ace Diamonds}?
-        //Depends on the context of the round.
+
 
         int thisCardsStrength = this.calculateCardStrength(trump);
         int otherCardsStrength = otherCard.calculateCardStrength(trump);
 
-        return Integer.compare(thisCardsStrength, otherCardsStrength); // -1,0,1
-        //this is the best we can do for bots. Could look into finding better ways
+        if(thisCardsStrength != otherCardsStrength)
+            return thisCardsStrength < otherCardsStrength ? -1 : 1; //Return -1 if this card is less than the other, else return 1
+
+        //if the cards have the same strength value (i.e. theyre both in the same range of cards)
+        //then we need to analyse further
+        if(thisCardsStrength == 1){
+            //both cards are non trump numbers
+            return compareCardsBasedOnValue(new String[]{"7","8","9","10"}, otherCard);
+        }
+        else if(thisCardsStrength == 2){
+            //both cards either non trump royals or trump numbers
+
+            //the following order is going to place preference on non trump royals over trump numbers.
+            //this might need to be tweaked?
+            return compareCardsBasedOnValue(new String[]{"A","K","Q","J","10","9","8","7"}, otherCard);
+
+        }
+        else if(thisCardsStrength == 3){
+            //both cards are royal trumps
+            return compareCardsBasedOnValue(new String[]{"A","K","Q"}, otherCard);
+
+        }
+        else{ //Card strength is 4
+            //both cards are bowers
+            if(this.equals(trump, "J")) //if this card is the right bower, its the higher card. return 1
+                return 1;
+            else
+                return -1; // else this card must be the left bower
+
+        }
+    }
+
+    private int compareCardsBasedOnValue(String[] order, Card otherCard){
+        int thisCardsPositionInArray = positionInStringArray(order, this.value);
+        int otherCardsPositionInArray = positionInStringArray(order, otherCard.getValue());
+
+        return Integer.compare(thisCardsPositionInArray, otherCardsPositionInArray);
+
+    }
+
+    private int positionInStringArray(String[] array, String key){
+
+        for(int i = 0; i < array.length;i++){
+            if(array[i].equals(key))
+                return i;
+        }
+        return -1;
     }
 }
 
